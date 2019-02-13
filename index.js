7292b0ed1a9c8fbe1df3f4f0c10383189a318063
sinchang <sinchangwen@gmail.com>
2019-02-13 14:58:03 +0800

    Add prettier

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const todayStr = dateFns.format(new Date(), 'YYYY-MM-DD')
 
 cityArr.forEach(city => {
   const url = `http://metro.sinchang.me/api/flows?city=${city}&count=1`
-  axios(url).then(res => {
-    if (res.data[0].date !== todayStr) {
-      throw new Error(`Not found ${city}'s data in ${todayStr}`)
-    }
-  }).catch(err => {
-    throw new Error(err)
-  })
+  axios(url)
+    .then(res => {
+      if (res.data[0].date !== todayStr) {
+        throw new Error(`Not found ${city}'s data in ${todayStr}`)
+      }
+    })
+    .catch(err => {
+      throw new Error(err)
+    })
 })
